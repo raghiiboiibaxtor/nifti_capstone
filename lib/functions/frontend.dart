efd7c808b3309708d50a2af77bf27d2c0d5a5fc7
Tess <tessrexx@yahoo.com>
2023-08-12 22:41:08 +1200

    Contact Card UI

---
+++ lib/functions/frontend.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// ? Error Message Snackbar Function
+void displayErrorMessage(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(99, 145, 255, 1)),
+      ),
+      duration: const Duration(seconds: 5),
+      width: 300.0, // Width of the SnackBar.
+      padding: const EdgeInsets.all(
+        10.0, // Inner padding for SnackBar content.
+      ),
+      backgroundColor: const Color.fromRGBO(252, 250, 245, 1),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      )));
+}
+
+// ? Loading Circle Animation Function
+void displayLoadingCircle(
+  BuildContext context,
+) {
+  showDialog(
+    context: context,
+    builder: (context) => const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+}
